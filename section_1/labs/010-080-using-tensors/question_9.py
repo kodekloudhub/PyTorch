1409ec738c3d042eaf2fc2b408e650b46529a742
geekbass <wbassler23@gmail.com>
2024-10-09 19:56:47 -0400

    added some additional information for working with GPUs

---
+++ section_1/labs/010-080-using-tensors/question_9.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+import torch
+
+# Check if GPU is available to use
+if torch.____.____:
+    device = '____'
+else:
+    device = '____'
+
+# Create a tensor and the device congifured above
+tensor = torch.tensor([4, 90, 90], device=device)
+
+# Print the device of the tensor
+print(tensor.device)
