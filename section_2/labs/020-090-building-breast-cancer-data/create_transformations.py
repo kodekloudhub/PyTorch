07548bdee73845c17224bee06fc01b5b2b983b1e
geekbass <wbassler23@gmail.com>
2024-11-14 10:04:24 -0500

    updating instructions for lab

--- section_2/labs/020-090-building-breast-cancer-data/create_transformations.py
+++ section_2/labs/020-090-building-breast-cancer-data/create_transformations.py
@@ -2,16 +2,18 @@
 Define `train_transform` and `val_transform` pipelines. 
 
 For the `train_transform` we need the following in our pipeline in the following order: 
-Resize of 256 x 256 pixels
+Resize of 128 x 128 pixels
+Set to Grayscale
+30 degrees random rotation
 50% chance of a random horizontal flip
 converted to a tensor
-normalized to mean and standard deviation of 0.5 across the board
+normalized to mean and standard deviation of 0.5 for 1 channel
 
 For the `val_transform` we need the following: 
-Resize of 256 x 256 pixels
+Resize of 128 x 128 pixels
+Set to Grayscale
 converted to a tensor
-and normalized to mean
-standard deviation of 0.5 across the board
+normalized to mean andstandard deviation of 0.5 for 1 channel 
 
 Use V2 API
 """
@@ -20,15 +22,18 @@ from ____ import ____
 
 # Train Pipeline
 ____ = ____.____([
-    ____.____((____, ____)), 
+    ____.____((____, ____)),
+    ____.____(),
+    ____.____(degrees=____),
     ____.____(p=____),
-    ____.____, 
-    ____.____(mean=[____, ____, ____], std=[____, ____, ____])
+    ____.____(),
+    ____.____((____), (____))
 ])
 
 # Validation Pipeline. Hint: Copy
-____ = ____.Compose([
+____ = ____.____([
     ____.____((____, ____)),
-    ____.____, 
-    ____.____(mean=[____, ____, ____], std=[____, ____, ____])
+    ____.____(),
+    ____.____(),
+    ____.____((____), (____))
 ])
