ab776b5a0ed32707c4c586ada67b091147cd90a6
geekbass <wbassler23@gmail.com>
2024-11-20 19:26:04 -0500

    fixing transformations in the lab

--- section_2/labs/020-090-building-breast-cancer-data/create_transformations.py
+++ section_2/labs/020-090-building-breast-cancer-data/create_transformations.py
@@ -7,13 +7,13 @@ Set to Grayscale
 30 degrees random rotation
 50% chance of a random horizontal flip
 converted to a tensor
-normalized to mean and standard deviation of 0.5 for 1 channel
+normalized mean (0.485, 0.456, 0.406) and standard deviation (0.229, 0.224, 0.225) for 3 channels
 
 For the `val_transform` we need the following: 
 Resize of 128 x 128 pixels
 Set to Grayscale
 converted to a tensor
-normalized to mean andstandard deviation of 0.5 for 1 channel 
+normalized mean (0.485, 0.456, 0.406) and standard deviation (0.229, 0.224, 0.225) for 3 channels
 
 Use V2 API
 """
@@ -23,17 +23,17 @@ from ____ import ____
 # Train Pipeline
 ____ = ____.____([
     ____.____((____, ____)),
-    ____.____(),
     ____.____(degrees=____),
     ____.____(p=____),
     ____.____(),
-    ____.____((____), (____))
+    ____.____(mean=[____, ____, ____], 
+              std=[____, ____, ____])
 ])
 
 # Validation Pipeline. Hint: Copy
 ____ = ____.____([
     ____.____((____, ____)),
     ____.____(),
-    ____.____(),
-    ____.____((____), (____))
+    ____.____(mean=[____, ____, ____], 
+              std=[____, ____, ____])
 ])
